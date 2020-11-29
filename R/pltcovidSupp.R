5dbd59c291780902fc2f0ea8f8f865ddae1a64dc
Jiyoung Kang <jiyoungkang01@gmail.com>
2020-11-29 16:23:03 +0900

    update

--- R/pltcovidSupp.R
+++ R/pltcovidSupp.R
@@ -1,4 +1,4 @@
-#' @title pltCovidInf
+#' @title pltcovidSupp
 #' @description plot function for CovidInf data
 #' @param CovidInf name of dataset
 #' @param plotopt options for plot
@@ -6,7 +6,7 @@
 #' @details DETAILS
 #' @examples
 #'  library(overcomeCovidKor)
-#'  pltCovidInf(covidSupp)
+#'  pltcovidSupp(covidSupp)
 #' @seealso
 #'  \code{\link[dplyr]{group_by}}
 #' @rdname pltCovidInf
@@ -16,11 +16,11 @@
 #' @import ggplot2
 
 
-pltCovidInf <- function(CovidInf, plotopt = 1) {
+pltcovidSupp <- function(covidSupp, plotopt = 1) {
   loan.krw1mil <- startup.year <- mean_loan.krw1mil  <- NULL
   industry <- month <- region <- NULL
 
-  df_loan.krw1mil <- CovidInf %>%
+  df_loan.krw1mil <- covidSupp %>%
     dplyr::group_by(startup.year) %>%
     dplyr::summarise(mean_loan.krw1mil = mean(loan.krw1mil))
 
@@ -31,12 +31,12 @@ pltCovidInf <- function(CovidInf, plotopt = 1) {
       x = stats::reorder(startup.year, -mean_loan.krw1mil),
       y = mean_loan.krw1mil
     )) + geom_col()
-  } else if (plotopt == 1) {
-    ggplot(data = CovidInf, aes(x = month, y = loan.krw1mil)) + geom_line()
   } else if (plotopt == 2) {
-    ggplot(data = CovidInf, aes(x = reorder(industry,-loan.krw1mil), y = loan.krw1mil)) + geom_boxplot()
+    loan.industry <- ggplot(data = covidSupp, aes(x = stats::reorder(industry, -loan.krw1mil), y = loan.krw1mil)) + geom_boxplot() + labs(x="Industry", y="Loan (KRW 1mil)") +theme(axis.text.x=element_text(angle=45, hjust=1))
   } else if (plotopt == 3) {
-    ggplot(data = CovidInf, aes(x = reorder(region,-loan.krw1mil), y = loan.krw1mil)) + geom_boxplot()
+    loan.region <- ggplot(data = covidSupp, aes(x = stats::reorder(region, -loan.krw1mil), y = loan.krw1mil)) + geom_boxplot() + labs(x="Region", y="Loan (KRW 1 mil)") +theme(axis.text.x=element_text(angle=45, hjust=1))
+  } else if (plotopt == 4) {
+    loan.month <- ggplot(data = covidSupp, aes(x = stats::reorder(month, -loan.krw1mil), y = loan.krw1mil)) + geom_boxplot() + labs(x="Month", y="Loan (KRW 1 mil)") +theme(axis.text.x=element_text(angle=45, hjust=1))
   }
 }
 
